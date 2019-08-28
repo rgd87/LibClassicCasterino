d9c4198282e72952aa3b6e52365ac7fe0907163b
d87 <d87d87@gmail.com>
2019-08-28 15:00:37 +0700

    stopping casts on death

--- LibClassicCasterino.lua
+++ LibClassicCasterino.lua
@@ -4,7 +4,7 @@ Author: d87
 --]================]
 
 
-local MAJOR, MINOR = "LibClassicCasterino", 7
+local MAJOR, MINOR = "LibClassicCasterino", 8
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 
@@ -180,6 +180,8 @@ function f:COMBAT_LOG_EVENT_UNFILTERED(event)
     elseif eventType == "SPELL_INTERRUPT" then
 
             CastStop(dstGUID, nil, "INTERRUPTED")
+    elseif eventType == "UNIT_DIED" then
+            CastStop(dstGUID, nil, "FAILED")
 
     elseif  eventType == "SPELL_AURA_APPLIED" or
             eventType == "SPELL_AURA_REFRESH" or
